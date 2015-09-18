9f2b29518aeb69a100c7516faec6d37d6e8d42e0
Brian Finck <brian@brianfinck.com>
2015-09-17 20:25:15 -0700

    added filtering to portfolio

--- public/js/main.js
+++ public/js/main.js
@@ -67,5 +67,30 @@ $(function() {
 		});
 		return false;
 	});
+	
+	var selectTags = function(event) {
+		event.preventDefault();
+
+		// Remove .active from all active tag links
+		$('[data-show-tag].active').removeClass('active');
+
+		// Add .active to the current tag link, and return contents of its attribute
+		var tag = $(event.currentTarget).addClass('active').data('show-tag');
+
+		if(tag == 'all') {
+			$('[data-tags]').show();
+			return;
+		}
+
+		// Hide ALL the tagged content, we'll un-hide next
+		$('[data-tags]').hide();
+
+		// Show all tagged content that CONTAINS tag
+		$('[data-tags*="' + tag + '"]').show();
+	};
+
+
+	// Our "tag" links
+	$('[data-show-tag]').click(selectTags);
 
 });
